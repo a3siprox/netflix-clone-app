858ebb923a5950908dbccf8248f948c8a09884bc
a3siprox <abdulmejidsetegn4@gmail.com>
2024-07-28 17:12:56 -0400

    netlify deployment

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import InstagramIcon from '@mui/icons-material/Instagram'
+import InstagramIcon from "@mui/icons-material/Instagram";
 import XIcon from "@mui/icons-material/X";
-import YouTubeIcon from '@mui/icons-material/Youtube'
+import YouTubeIcon from "@mui/icons-material/Youtube";
 const Footer = () => {
-  return (
+	return (
 		<>
 			<div className="footer_outer_container">
 				<div className="footer_inner_container">
@@ -54,6 +54,6 @@ const Footer = () => {
 			</div>
 		</>
 	);
-}
+};
 
-export default Footer
+export default Footer;
