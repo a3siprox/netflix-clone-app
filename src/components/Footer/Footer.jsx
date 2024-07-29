d603f4e77a6b717a0717754f4bcf837b9a03b824
a3siprox <abdulmejidsetegn4@gmail.com>
2024-07-29 05:20:13 -0400

    deploy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import XIcon from "@mui/icons-material/X";
-import YouTubeIcon from "@mui/icons-material/Youtube";
+// import YouTubeIcon from "@mui/icons-material/Youtube";
 const Footer = () => {
 	return (
 		<>
