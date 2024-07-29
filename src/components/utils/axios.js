83ffae9612a13f860c9d7cbe2c5943dc03e37efe
a3siprox <abdulmejidsetegn4@gmail.com>
2024-07-29 14:36:37 -0400

    deploying netflix

--- src/components/utils/axios.js
+++ src/components/utils/axios.js
@@ -1,7 +1,25 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+// 	baseURL: "https://api.themoviedb.org/3",
+// });
+
+// export default instance;
+
+// utils/axios.js
+// utils/axios.js
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://api.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+instance.interceptors.request.use((config) => {
+  config.params = config.params || {};
+  config.params['api_key'] = import.meta.env.VITE_TMDB_API_KEY;
+  return config;
 });
 
 export default instance;
+
+
--- src/components/utils/requests.js
+++ src/components/utils/requests.js
@@ -1,19 +1,16 @@
 // src/requests.js
-const API_KEY = import.meta.env.VITE_API_KEY;
-
 const requests = {
-	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-	fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrending: `/trending/all/week?language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?language=en-US`,
+  fetchActionMovies: `/discover/movie?with_genres=28`,
+  fetchComedyMovies: `/discover/movie?with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?with_genres=99`,
 };
 
 export default requests;
 
-const link = "https://api.themoviedb.org/3/movie/550?api_key=";
 
 // const API_KEY = "8b8e8f863fcfe9459aa0bcef9154e43f";
