a1372c50e93dc21181c98e05d8c656747f8168da
a3siprox <abdulmejidsetegn4@gmail.com>
2024-07-29 08:57:09 -0400

    deploy

--- Api/script.js
+++ Api/script.js
@@ -1,14 +1,12 @@
 // script.js
-const apiKey = "f34d2929209f218564cfc64918b0b0ad"; // replace with your API key
-const city = "Addis Ababa"; // replace with your city
-const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-'http://api.openweathermap.org/data/2.5/weather?q=london&appid=f34d2929209f218564cfc64918b0b0ad'
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27`;
 
 http: fetch(url)
 	.then((response) => response.json())
 	.then((data) => {
 		const weatherData = document.getElementById("weather-data");
-        const iconUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+		const iconUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 		weatherData.innerHTML = `
             <h2>Current Weather in ${city}</h2>
             <img src='${iconUrl}'/>
